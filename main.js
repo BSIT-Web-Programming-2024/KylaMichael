344c1ecebcaab910f69cf2820648fa184f625164
Kyla Michael <patchangkyla8@gmail.com>
2024-11-12 13:07:17 +0800

    Add files via upload
    Updated

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//word at home page 
 var typed = new Typed(".text", {
     strings:["Digital Artist" , "Student" , "Caregiver"],
     typeSpeed:100,
@@ -5,7 +6,9 @@ var typed = new Typed(".text", {
     backDelay:1000,
     loop:true
 });
-let menuIcon = document.querySelector('menu-icon');
+
+//hamburger menu icon 
+let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 let sections = document.querySelector('section');
 let navLinks = document.querySelector('header nav a');
@@ -13,7 +16,7 @@ let navLinks = document.querySelector('header nav a');
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop -150;
+        let offset = sec.offsetTop - 150;
         let hieght = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
@@ -29,3 +32,12 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle ('bx-x');
     navbar.classList.toggle('active');
     }
+
+
+//mouse movement or the cursor
+var cursor = document.querySelector(".cursor");
+var cursor2 = document.querySelector(".cursor2");
+document.addEventListener("mousemove",function(e){
+    cursor.style.cssText = cursor2.style.cssText ="left: " + e.clientX
+    + "px; top: " + e.clientY + "px;";
+});
